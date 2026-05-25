153e8c262688319e371ebc950fd3afeb5c3ac323
JayRocketMaN <odidikasomto@gmail.com>
2026-05-25 15:36:05 +0100

    typescript config

--- src/config/supabase.ts
+++ src/config/supabase.ts
@@ -1,10 +1,11 @@
 import { createClient } from '@supabase/supabase-js';
 import 'dotenv/config';
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY; // The sb_secret one
+// Add the '!' at the end of the variables
+const supabaseUrl = process.env.SUPABASE_URL!;
+const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY!; 
 
+// TypeScript will now allow this because it's guaranteed to be a string
 const supabase = createClient(supabaseUrl, supabaseKey);
-
 
 export default supabase;
