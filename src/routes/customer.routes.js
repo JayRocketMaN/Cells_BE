baba1dc25c0d53fd396849c6df22202062036dcd
JayRocketMaN <odidikasomto@gmail.com>
2026-06-30 12:05:54 +0100

    add cookie-parser to dev3

--- src/routes/customer.routes.js
+++ src/routes/customer.routes.js
@@ -5,7 +5,7 @@ import * as authMiddleware from '../middleware/auth.middleware.js';
 const router = Router();
 
 // 1. STATIC ROUTES (Specific paths first)
-router.get('/dashboard',customerController.getDashboard);
+router.get('/dashboard', authMiddleware.authMiddleware, customerController.getDashboard);
 router.get('/history', authMiddleware.authMiddleware, customerController.getAllTransactions); // MOVED UP HERE
 router.get('/search', customerController.searchCustomer); // MOVED UP HERE
 router.get('/status/:status', authMiddleware.authMiddleware, customerController.getByStatus);
